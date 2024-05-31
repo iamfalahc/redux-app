cd8a69848d363c542cbfd49c1d1cb64548bab6be
Muhammed Falah <iamfalahc@gmail.com>
2024-05-31 18:32:40 +0530

    store created

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import store from "../src/components/redux/store";
+import { Provider, provider } from "react-redux";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    {" "}
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>
 );
-
